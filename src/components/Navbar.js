db4f72d3d612d15158a9474405ad3efb782db561
Terfa John <118588520+Terfa-john@users.noreply.github.com>
2023-10-16 21:04:11 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
-// import dice_logo from '../logos/dice_logo.png';
 
 class Navbar extends Component {
-
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow text-monospace">
-       
         {/* <img src={dice_logo} height="32" alt="logo" /> */}
-         BETTING BLOCK CHAINGAME
-       
-        {!this.props.account ? <div id="loader" className="spinner-border text-light" role="status"></div> :
+        BETTING BLOCKCHAIN GAME
+        {!this.props.account ? (
+          <div id="loader" className="spinner-border text-light" role="status"></div>
+        ) : (
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <a
               className="text-white"
-              href={"https://rinkeby.etherscan.io/address/" + this.props.account}
+              href={`https://rinkeby.etherscan.io/address/${this.props.account}`}
               target="_blank"
               rel="noopener noreferrer"
             >
-              {(this.props.account)}
+              {this.props.account}
             </a>&nbsp;
           </li>
-        }
+        )}
       </nav>
     );
   }
 }
+
+export default Navbar;
 
 export default Navbar;
